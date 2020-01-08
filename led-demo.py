8146941e0522d2a647690fa67ff7f06456fd99f3
iiglesiasg <ningunpenk@hotmail.com>
2020-01-08 22:37:23 +0100

    dapr 1

--- led-demo.py
+++ led-demo.py
@@ -5,7 +5,7 @@ import os
 
 red = LED(26)
 
-dapr_url = os.getenv("DAPR_HTTP_PORT":"http://10.43.29.189:80/v1.0/bindings/measure-dapr")
+dapr_url = os.getenv("DAPR_HTTP_PORT","http://10.43.29.189:80/v1.0/bindings/measure-dapr")
 
 while True:
     payload = {"device": "pi",
