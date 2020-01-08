08ba90075fb778ca5a4cdde8eb1bb1315e0e449f
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 00:39:15 +0100

    python logs

--- led-demo.py
+++ led-demo.py
@@ -3,6 +3,7 @@ from time import sleep
 import requests
 import os
 
+print('Saludos Terricolas')
 red = LED(26)
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/bindings/sample-topic".format(dapr_port)
