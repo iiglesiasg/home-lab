de21be9bbdce16b502130e12269cab393b9f7bb0
iiglesiasg <ningunpenk@hotmail.com>
2020-01-08 22:26:21 +0100

    dapr 1

--- led-demo.py
+++ led-demo.py
@@ -1,10 +1,23 @@
-from gpiozero import LED
+from gpiozero import LED,CPUTemperature
 from time import sleep
+import requests
+import os
 
 red = LED(26)
 
+dapr_url = os.getenv("DAPR_HTTP_PORT":"http://10.43.29.189:80/v1.0/bindings/measure-dapr")
+
 while True:
-    red.on()
-    sleep(1)
+    payload = {"device": "pi",
+               "signalType": "Temp",
+               "magnitude": "Cº",
+               "value": CPUTemperature.value}
+    try:
+        response = requests.post(dapr_url, json=payload)
+        print(response.text, flush=True)
+        red.on()
+        sleep(1)
+    except Exception as e:
+        print(e)
     red.off()
-    sleep(1)
+    sleep(10)
