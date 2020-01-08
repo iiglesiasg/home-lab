31865d70f7001b59807e23ffc1f38851d970766e
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 00:27:58 +0100

    dapr component syntax solved

--- led-demo.py
+++ led-demo.py
@@ -4,9 +4,10 @@ import requests
 import os
 
 red = LED(26)
-
-dapr_url = os.getenv("DAPR_HTTP_PORT", 'http://10.43.29.189:80/v1.0/bindings/measure-dapr')
+dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
+dapr_url = "http://localhost:{}/v1.0/bindings/sample-topic".format(dapr_port)
 cpu = CPUTemperature(min_temp=50, max_temp=90)
+print('Initial temperature: {}C'.format(cpu.temperature))
 
 while True:
     payload = {"device": "pi",
