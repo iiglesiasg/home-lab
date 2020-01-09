f15c88937db749374343bcef0d86e8b54d61ca33
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 13:26:22 +0100

    python error

--- led-demo.py
+++ led-demo.py
@@ -4,21 +4,17 @@ import requests
 import logging
 import os
 
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('This will get logged')
-print('Saludos Terricolas')
 red = LED(26)
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/bindings/measure-dapr".format(dapr_port)
 cpu = CPUTemperature(min_temp=50, max_temp=90)
 logging.info('Initial temperature: {}C'.format(cpu.temperature))
-logging.debug('This will get logged 2')
+
 while True:
-    logging.debug('This will get logged 3')
-    payload = {"device": "pi",
+    payload = {"data": {"device": "pi",
                "signalType": "Temp",
                "magnitude": "Cº",
-               "value": cpu.temperature}
+               "value": cpu.temperature}}
     try:
         response = requests.post(dapr_url, json=payload)
         print(response.text, flush=True)
