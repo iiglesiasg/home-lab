5e01fe8bf528f4ce06ea400fd2738b289ca04d99
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 10:50:29 +0100

    python logs

--- led-demo.py
+++ led-demo.py
@@ -11,7 +11,7 @@ red = LED(26)
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/bindings/measure-dapr".format(dapr_port)
 cpu = CPUTemperature(min_temp=50, max_temp=90)
-print('Initial temperature: {}C'.format(cpu.temperature))
+logging.info('Initial temperature: {}C'.format(cpu.temperature))
 logging.debug('This will get logged 2')
 while True:
     logging.debug('This will get logged 3')
@@ -26,5 +26,6 @@ while True:
         sleep(1)
     except Exception as e:
         print(e)
+        logging.error(response.text)
     red.off()
     sleep(10)
