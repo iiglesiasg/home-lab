6d943106117ea5c2dfd7b04302860bb67de195bf
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 10:04:57 +0100

    python logs

--- led-demo.py
+++ led-demo.py
@@ -6,7 +6,7 @@ import os
 print('Saludos Terricolas')
 red = LED(26)
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
-dapr_url = "http://localhost:{}/v1.0/bindings/sample-topic".format(dapr_port)
+dapr_url = "http://localhost:{}/v1.0/bindings/measure-dapr".format(dapr_port)
 cpu = CPUTemperature(min_temp=50, max_temp=90)
 print('Initial temperature: {}C'.format(cpu.temperature))
 
