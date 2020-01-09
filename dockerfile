aab8c16e375a9017c781dbf0430488fa63e75301
iiglesiasg <ningunpenk@hotmail.com>
2020-01-09 10:19:14 +0100

    python logs

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
 FROM arm32v7/python:3.7-buster 
 ADD led-demo.py /
-RUN pip3 install --no-cache-dir gpiozero && pip3 install requests
+RUN pip3 install --no-cache-dir gpiozero  requests logging
 CMD [ "python3", "./led-demo.py" ]
--- led-demo.py
+++ led-demo.py
@@ -1,16 +1,20 @@
 from gpiozero import LED,CPUTemperature
 from time import sleep
 import requests
+import logging
 import os
 
+logging.basicConfig(level=logging.DEBUG)
+logging.debug('This will get logged')
 print('Saludos Terricolas')
 red = LED(26)
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/bindings/measure-dapr".format(dapr_port)
 cpu = CPUTemperature(min_temp=50, max_temp=90)
 print('Initial temperature: {}C'.format(cpu.temperature))
-
+logging.debug('This will get logged 2')
 while True:
+    logging.debug('This will get logged 3')
     payload = {"device": "pi",
                "signalType": "Temp",
                "magnitude": "Cº",
