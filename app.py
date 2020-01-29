6dc5e81a85f0243af8aa373d901396f6acd94a19
iiglesiasg <ningunpenk@hotmail.com>
2020-01-29 17:40:29 +0100

    first commit

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main():
     red = LED(26)
 
     apm = ElasticAPM(app, service_name='led-demo', secret_token='z9lp5srpkxs2jn5gknzvr8ml', logging=True)
-    client = Client({'SERVICE_NAME': 'example'}, **defaults)
+    client = Client({'SERVICE_NAME': 'led-demo'})
     dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
     dapr_url = "http://localhost:{}/v1.0/bindings/measure-dapr".format(dapr_port)
     cpu = CPUTemperature(min_temp=50, max_temp=90)
